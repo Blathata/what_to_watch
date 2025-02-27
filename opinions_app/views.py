9b6f2f1139e3fff6aeb74ac8560170e7a4c52be9
Brezhnev Ivan <IBrezhnev@mosbrew.ru>
2025-02-27 05:12:26 +0300

    3

--- opinions_app/views.py
+++ opinions_app/views.py
@@ -1,49 +1,10 @@
-# what_to_watch/opinions_app.py
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField, URLField
-from wtforms.validators import DataRequired, Length, Optional
-from datetime import datetime
 from random import randrange
-from flask_migrate import Migrate
 
-# Импортируем функцию render_template():
-from flask import Flask, abort, flash, redirect, render_template, url_for
-from flask_sqlalchemy import SQLAlchemy
+from flask import abort, flash, redirect, render_template, url_for
 
-app = Flask(__name__, static_folder='static')
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
-app.config['SECRET_KEY'] = '5867076'
-
-db = SQLAlchemy(app)
-migrate = Migrate(app, db)
-
-
-class Opinion(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(128), nullable=False)
-    text = db.Column(db.Text, unique=True, nullable=False)
-    source = db.Column(db.String(256))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    # Новое поле.
-    added_by = db.Column(db.String(64))
-
-
-class OpinionForm(FlaskForm):
-    title = StringField(
-        'Введите название фильма',
-        validators=[DataRequired(message='Обязательное поле'),
-                    Length(1, 128)]
-    )
-    text = TextAreaField(
-        'Напишите мнение',
-        validators=[DataRequired(message='Обязательное поле')]
-    )
-    source = URLField(
-        'Добавьте ссылку на подробный обзор фильма',
-        validators=[Length(1, 256), Optional()]
-    )
-    submit = SubmitField('Добавить')
+from . import app, db
+from .forms import OpinionForm
+from .models import Opinion
 
 
 @app.route('/')
@@ -100,28 +61,3 @@ def opinion_view(id):
     opinion = Opinion.query.offset(offset_value).first()
     # ...и передать его в шаблон (шаблон тот же, что и для главной страницы):
     return render_template('opinion.html', opinion=opinion)
-
-
-# Тут декорируется обработчик и указывается код нужной ошибки:
-@app.errorhandler(500)
-def internal_error(error):
-    # Ошибка 500 возникает в нештатных ситуациях на сервере.
-    # Например, провалилась валидация данных.
-    # В таких случаях можно откатить изменения, не зафиксированные в БД,
-    # чтобы в базу не записалось ничего лишнего.
-    db.session.rollback()
-    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
-    # Этого шаблона пока нет, но сейчас мы его тоже создадим.
-    # Пользователь получит и код HTTP-ответа 500.
-    return render_template('500.html'), 500
-
-
-@app.errorhandler(404)
-def page_not_found(error):
-    # При ошибке 404 в качестве ответа вернётся страница, созданная
-    # на основе шаблона 404.html, и код HTTP-ответа 404:
-    return render_template('404.html'), 404
-
-
-if __name__ == '__main__':
-    app.run()
