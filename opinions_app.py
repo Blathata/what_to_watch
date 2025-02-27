9e7be5bd4fd950f446b568d8f810cef03284e97b
Brezhnev Ivan <IBrezhnev@mosbrew.ru>
2025-02-27 03:50:49 +0300

    d

--- opinions_app.py
+++ opinions_app.py
@@ -4,9 +4,10 @@ from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 from datetime import datetime
 from random import randrange
+from flask_migrate import Migrate
 
 # Импортируем функцию render_template():
-from flask import Flask, redirect, render_template, url_for
+from flask import Flask, abort, flash, redirect, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__, static_folder='static')
@@ -15,6 +16,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
 app.config['SECRET_KEY'] = '5867076'
 
 db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class Opinion(db.Model):
@@ -23,6 +25,8 @@ class Opinion(db.Model):
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    # Новое поле.
+    added_by = db.Column(db.String(64))
 
 
 class OpinionForm(FlaskForm):
@@ -58,6 +62,13 @@ def add_opinion_view():
     # Вот тут создаётся новый экземпляр формы:
     form = OpinionForm()
     if form.validate_on_submit():
+        text = form.text.data
+        # Если в БД уже есть мнение с текстом, который ввёл пользователь...
+        if Opinion.query.filter_by(text=text).first() is not None:
+            # ...вызвать функцию flash и передать соответствующее сообщение:
+            flash('Такое мнение уже было оставлено ранее!')
+            # Вернуть пользователя на страницу «Добавить новое мнение»:
+            return render_template('add_opinion.html', form=form)
         # ...то нужно создать новый экземпляр класса Opinion:
         opinion = Opinion(
             # И передать в него данные, полученные из формы:
@@ -79,10 +90,37 @@ def add_opinion_view():
 @app.route('/opinions/<int:id>')
 # Параметром указывается имя переменной:
 def opinion_view(id):
+    quantity = Opinion.query.count()
+    if not quantity:
+        # Если в базе пусто, при запросе к главной странице
+        # пользователь увидит ошибку 500:
+        abort(500)
+    offset_value = randrange(quantity)
     # Теперь можно запросить нужный объект по id...
-    opinion = Opinion.query.get_or_404(id)
+    opinion = Opinion.query.offset(offset_value).first()
     # ...и передать его в шаблон (шаблон тот же, что и для главной страницы):
     return render_template('opinion.html', opinion=opinion)
+
+
+# Тут декорируется обработчик и указывается код нужной ошибки:
+@app.errorhandler(500)
+def internal_error(error):
+    # Ошибка 500 возникает в нештатных ситуациях на сервере.
+    # Например, провалилась валидация данных.
+    # В таких случаях можно откатить изменения, не зафиксированные в БД,
+    # чтобы в базу не записалось ничего лишнего.
+    db.session.rollback()
+    # Пользователю вернётся страница, сгенерированная на основе шаблона 500.html.
+    # Этого шаблона пока нет, но сейчас мы его тоже создадим.
+    # Пользователь получит и код HTTP-ответа 500.
+    return render_template('500.html'), 500
+
+
+@app.errorhandler(404)
+def page_not_found(error):
+    # При ошибке 404 в качестве ответа вернётся страница, созданная
+    # на основе шаблона 404.html, и код HTTP-ответа 404:
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
